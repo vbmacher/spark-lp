5ea45c84d7632fb1805c99375b6b0ba8b69f735d
Peter Jakubco <pjakubco@gmail.com>
2025-01-03 18:12:14 +0100

    Removed licenses and authors from files
    + Fixed log4j and log4j2 properties
    + Added AUTHORS
    + Added CODE_OF_CONDUCT.md

--- spark-lp/src/main/scala/org/apache/spark/mllib/optimization/lp/fs/dmatrix/vector/LPRowMatrix.scala
+++ spark-lp/src/main/scala/org/apache/spark/mllib/optimization/lp/fs/dmatrix/vector/LPRowMatrix.scala
@@ -1,26 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
-  * @author Ehsan Mohyedin Kermani: ehsanmo1367@gmail.com
-  * Appropriate modifications of
-  * @see [[https://github.com/apache/spark/blob/master/mllib/src/main/scala/org/apache/spark/mllib/linalg/distributed/RowMatrix.scala]]
-  */
-
 package org.apache.spark.mllib.optimization.lp.fs.dmatrix.vector
 
 import breeze.linalg.{DenseVector => BDV}
@@ -31,6 +8,9 @@ import org.apache.spark.storage.StorageLevel
 
 /**
   * Represents a row-oriented distributed Matrix with no meaningful row indices.
+  *
+  * Appropriate modifications of
+  * @see [[https://github.com/apache/spark/blob/master/mllib/src/main/scala/org/apache/spark/mllib/linalg/distributed/RowMatrix.scala]]
   *
   * @param rows  The rows stored as an RDD[Vector]
   * @param nRows The number of rows. A non-positive value means unknown, and then the number of rows will

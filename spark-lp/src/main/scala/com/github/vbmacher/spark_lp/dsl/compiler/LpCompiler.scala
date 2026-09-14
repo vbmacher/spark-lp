831a23364b602a115bcdbe1e127e0c9bcdb65c8c
Peter Jakubco <pjakubco@gmail.com>
2026-09-14 11:48:26 +0200

    [#37] Solve feasibility models with an implicit zero objective

--- spark-lp/src/main/scala/com/github/vbmacher/spark_lp/dsl/compiler/LpCompiler.scala
+++ spark-lp/src/main/scala/com/github/vbmacher/spark_lp/dsl/compiler/LpCompiler.scala
@@ -104,8 +104,7 @@ private[dsl] final class LpCompiler(problem: LpProblem, config: SolveConfig) ext
     * coefficient matrix is never densified.
     */
   private[dsl] def compile(): Compiled = {
-    val objective = problem.objective.getOrElse(
-      fail(s"Problem '${problem.name}' has no objective; add one with += or setObjective"))
+    val objective = problem.objective.getOrElse(LpExpr.zero)
     if (notFinite(objective.constant)) {
       fail(s"Problem '${problem.name}': non-finite objective constant ${objective.constant}")
     }
